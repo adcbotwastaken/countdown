659249cfa3e5b446ba30263f8d3342f0aee1f719
Timothy <lai.timothy77@gmail.com>
2021-09-03 18:49:14 -0400

    fuctionality is more or less there, tracks the date and updates the progress bar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import CountdownCard from './components/CountdownCard';
+import Clock from './components/Clock'
+import { Grid } from '@material-ui/core';
+
 
 function App() {
+
+
+  //ISO Date with year should be YYYY-MM-DDTHH:MM:SSZ):
+  // for later: https://www.w3schools.com/js/js_date_formats.asp
+  const date1 = {
+    "name": "Not Friends",
+    "startDate": '01 Sep 2021 00:00:00 EST',
+    "endDate": '2021-09-03T18:00:00.000+09:00'
+  };
+
+  const date2 = {
+    "name" : "Hula Hoop",
+    "startDate": '01 Sep 2021 00:00:00 EST',
+    "endDate": '2021-09-15T18:00:00.000+09:00'
+  };
+
+  const date3 = {
+    "name" : "testing",
+    "startDate" : "2021-09-03T15:39:00",
+    "endDate": "2021-09-03T19:59:00",
+  }
+
+  
+  const date4 = {
+    "name" : "test2",
+    "startDate" : "2021-09-03T18:00:00",
+    "endDate": "2021-09-03T18:30:00",
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid>
+        <Clock />
+        <CountdownCard date={date1} />
+        <CountdownCard date={date2} />
+        <CountdownCard date={date3} />
+        <CountdownCard date={date4} />
+      </Grid>
+
     </div>
   );
 }
---
+++ src/components/CountdownCard.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles'
+import {
+    Card,
+    CardContent,
+    Typography,
+    LinearProgress
+} from '@material-ui/core';
+
+
+
+const useStyles = makeStyles({
+    cardStyle: {
+        padding: "15px",
+        borderRadius: 32,
+        minHeight: '15vw',
+        maxHeight: '30vw',
+        overflow: 'auto',
+
+    }
+});
+
+
+function CountdownCard(props) {
+    // const startDate = props.startDate;
+    // const endDate = props.endDate;
+
+    //console.log(props.date.endDate);
+    //console.log(props.date.startDate);
+
+    // strings of the name, and start and end dates in (hopefully) correct formats
+    const name = props.date.name;
+    const startDateStr = props.date.startDate;
+    const endDateStr = props.date.endDate;
+
+    // values are in milliseconds
+    const startDate = Date.parse(startDateStr);
+    const endDate = Date.parse(endDateStr);
+    const endDateObj = new Date(endDate);
+    const totalTime = endDate - startDate;
+
+
+    // barProgress should be a percentage as a number(50 = 50%, 24.5 = 24.5%)
+    const [barProgress, setBarProgress] = useState(0);
+    //const [eventPassed, setEventPassed] = useState(false);
+    //console.log((Date.now() / endDate));
+
+
+    // updates the countdown every half second.
+    useEffect(() => {
+        setInterval(() => {
+            // check if the date has already passed and fill out the progress bar else update normally
+            if(calculateProgress() >= 100){
+                setBarProgress(100);
+                //setEventPassed(true);
+            }else{
+                setBarProgress(calculateProgress());
+            }
+ 
+        }, 500);
+    });
+
+
+
+    // returns the progress of the countdown
+    function calculateProgress() {
+        return ((Date.now() - startDate) / totalTime) * 100;
+    }
+
+    // const timeLeftStr = calculateProgress();
+    const cardText = `${name} is out on ${endDateObj.getFullYear()}.${endDateObj.getMonth()}.${endDateObj.getDate()}`;
+
+    
+
+
+    return(
+        <Card>
+            <Typography>{cardText}</Typography>
+            <LinearProgress variant="determinate" value={barProgress} />
+        </Card>
+    );
+
+
+    
+}
+
+
+export default CountdownCard;
