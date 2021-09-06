2e4155663b96f91749c59a119dcb74c587f345a6
Timothy <lai.timothy77@gmail.com>
2021-09-06 19:00:22 -0400

    should create the countdown cards based on the array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import CountdownCard from './components/CountdownCard';
 import Clock from './components/Clock'
-import { Grid } from '@material-ui/core';
+import { Grid, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  boxStyle: {
+    paddingTop: '5%',
+    paddingBottom: '5%',
+    paddingLeft: '5%',
+    paddingRight: '5%'
+  }
+})
+
+
 
 
 function App() {
-
-
   //ISO Date with year should be YYYY-MM-DDTHH:MM:SSZ):
   // for later: https://www.w3schools.com/js/js_date_formats.asp
   const date1 = {
@@ -27,26 +37,69 @@ function App() {
     "endDate": "2021-09-03T19:59:00",
   }
 
-  
   const date4 = {
     "name" : "test2",
     "startDate" : "2021-09-03T18:00:00",
     "endDate": "2021-09-03T18:30:00",
   }
 
+  const date5 = {
+    "name": "End of Year",
+    "startDate": "2021-01-01T00:00:00",
+    "endDate": "2022-01-01T00:00:00",
+  }
 
 
+  const datesArray = [date1, date2, date3, date4, date5];
+
+
+  const classes = useStyles();
   return (
-    <div>
-      <Grid>
-        <Clock />
-        <CountdownCard date={date1} />
-        <CountdownCard date={date2} />
-        <CountdownCard date={date3} />
-        <CountdownCard date={date4} />
+    <Box className={classes.boxStyle}>
+      <Grid
+        container
+        spacing={2}
+        direction='column'
+        justifyContent='flex-start'
+        alignItems='stretch'
+      >
+        <Grid item xs={12} spacing={3}>
+          <Clock />
+        </Grid>
+      <Grid container spacing={3}>
+        {datesArray.map(dateObj => {
+          return(
+            <Grid item xs={12}>
+              <CountdownCard date={dateObj}/>
+            </Grid>
+          );
+        })}
       </Grid>
 
-    </div>
+
+
+
+
+
+
+        {/* <Grid container>
+          <Grid item xs={12}>
+            <CountdownCard date={date1} />
+          </Grid>
+          <Grid item xs={12}>
+            <CountdownCard date={date2} />
+          </Grid>
+          <Grid item xs={12}>
+            <CountdownCard date={date3} />
+          </Grid>
+          <Grid item xs={12}>
+            <CountdownCard date={date4} />
+          </Grid>
+
+        </Grid> */}
+      </Grid>
+
+    </Box>
   );
 }
 
