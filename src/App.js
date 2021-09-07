b9a172b72e121769d24f917fa90a7e1e3fe5f9df
Timothy <lai.timothy77@gmail.com>
2021-09-06 21:23:46 -0400

    moved the dates to a seperate file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import CountdownCard from './components/CountdownCard';
 import Clock from './components/Clock'
 import { Grid, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import countdownDates from './data/countdownDates.js';
+
 
 const useStyles = makeStyles({
   boxStyle: {
@@ -14,44 +16,9 @@ const useStyles = makeStyles({
 })
 
 
-
-
 function App() {
   //ISO Date with year should be YYYY-MM-DDTHH:MM:SSZ):
   // for later: https://www.w3schools.com/js/js_date_formats.asp
-  const date1 = {
-    "name": "Not Friends",
-    "startDate": '01 Sep 2021 00:00:00 EST',
-    "endDate": '2021-09-03T18:00:00.000+09:00'
-  };
-
-  const date2 = {
-    "name" : "Hula Hoop",
-    "startDate": '01 Sep 2021 00:00:00 EST',
-    "endDate": '2021-09-15T18:00:00.000+09:00'
-  };
-
-  const date3 = {
-    "name" : "testing",
-    "startDate" : "2021-09-03T15:39:00",
-    "endDate": "2021-09-03T19:59:00",
-  }
-
-  const date4 = {
-    "name" : "test2",
-    "startDate" : "2021-09-03T18:00:00",
-    "endDate": "2021-09-03T18:30:00",
-  }
-
-  const date5 = {
-    "name": "End of Year",
-    "startDate": "2021-01-01T00:00:00",
-    "endDate": "2022-01-01T00:00:00",
-  }
-
-
-  const datesArray = [date1, date2, date3, date4, date5];
-
 
   const classes = useStyles();
   return (
@@ -67,7 +34,7 @@ function App() {
           <Clock />
         </Grid>
       <Grid container spacing={3}>
-        {datesArray.map(dateObj => {
+        {countdownDates.map(dateObj => {
           return(
             <Grid item xs={12}>
               <CountdownCard date={dateObj}/>
@@ -75,28 +42,6 @@ function App() {
           );
         })}
       </Grid>
-
-
-
-
-
-
-
-        {/* <Grid container>
-          <Grid item xs={12}>
-            <CountdownCard date={date1} />
-          </Grid>
-          <Grid item xs={12}>
-            <CountdownCard date={date2} />
-          </Grid>
-          <Grid item xs={12}>
-            <CountdownCard date={date3} />
-          </Grid>
-          <Grid item xs={12}>
-            <CountdownCard date={date4} />
-          </Grid>
-
-        </Grid> */}
       </Grid>
 
     </Box>
---
+++ src/data/countdownDates.js
@@ -0,0 +1,21 @@
+const countdownDates = [
+    {
+        "name": "Not Friends",
+        "startDate": '01 Sep 2021 00:00:00 EST',
+        "endDate": '2021-09-03T18:00:00.000+09:00'
+    },
+
+    {
+        "name": "Hula Hoop",
+        "startDate": '01 Sep 2021 00:00:00 EST',
+        "endDate": '2021-09-15T18:00:00.000+09:00'
+    },
+
+    {
+        "name": "End of Year",
+        "startDate": "2021-01-01T00:00:00",
+        "endDate": "2022-01-01T00:00:00",
+    }
+]
+
+export default countdownDates;
