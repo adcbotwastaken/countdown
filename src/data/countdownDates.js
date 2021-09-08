b8af6ea3568391bf0ea92fc53775e2f10d539f74
Timothy <lai.timothy77@gmail.com>
2021-09-08 08:25:11 -0400

    updated data with another dateobj

--- src/data/countdownDates.js
+++ src/data/countdownDates.js
@@ -1,14 +1,20 @@
 const countdownDates = [
     {
-        "name": "Not Friends",
+        "name": "LOONA: Not Friends",
         "startDate": '01 Sep 2021 00:00:00 EST',
         "endDate": '2021-09-03T18:00:00.000+09:00'
     },
 
     {
-        "name": "Hula Hoop",
+        "name": "LOONA: Hula Hoop",
         "startDate": '01 Sep 2021 00:00:00 EST',
         "endDate": '2021-09-15T18:00:00.000+09:00'
+    },
+
+    {
+        "name": "TWICE: The Feels",
+        "startDate": '23 Aug 2021 00:00:00 EST',
+        "endDate": '2021-10-01T13:00:00.000+09:00'
     },
 
     {
@@ -16,6 +22,8 @@ const countdownDates = [
         "startDate": "2021-01-01T00:00:00",
         "endDate": "2022-01-01T00:00:00",
     }
+
+    
 ]
 
 export default countdownDates;
