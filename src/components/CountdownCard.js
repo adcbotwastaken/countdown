6695186d6454c05d9dc947dfe3f1f23a3937054e
Timothy <lai.timothy77@gmail.com>
2021-09-13 22:18:32 -0400

    countdown timer implemented

--- src/components/CountdownCard.js
+++ src/components/CountdownCard.js
@@ -40,6 +40,7 @@ function CountdownCard(props) {
 
     // barProgress should be a percentage as a number(50 = 50%, 24.5 = 24.5%)
     const [barProgress, setBarProgress] = useState(0);
+    const [timeLeft, setTimeLeft] = useState(0);
     //const [eventPassed, setEventPassed] = useState(false);
     //console.log((Date.now() / endDate));
 
@@ -50,9 +51,11 @@ function CountdownCard(props) {
             // check if the date has already passed and fill out the progress bar else update normally
             if(calculateProgress() >= 100){
                 setBarProgress(100);
+                setTimeLeft("Countdown Finished!");
                 //setEventPassed(true);
             }else{
                 setBarProgress(calculateProgress());
+                setTimeLeft(getTimeLeftStr(endDateObj));
             }
  
         }, 500);
@@ -65,6 +68,8 @@ function CountdownCard(props) {
         return ((Date.now() - startDate) / totalTime) * 100;
     }
 
+
+
     // const timeLeftStr = calculateProgress();
 
 
@@ -74,17 +79,51 @@ function CountdownCard(props) {
     const dateString = `${endDateObj.getFullYear()}.${monthFormatted}.${dateFormatted}`
 
     
-    const cardText = `${name} on ${dateString}`;
+    const cardNameText = `${name}`;
+    const cardDateText = `${dateString}`;
+
+
+    //const cardTimeLeftText = ''
 
     
 
 
     return(
         <Card>
-            <Typography>{cardText}</Typography>
+            <Typography>{cardNameText} on {cardDateText}</Typography>
+            <Typography>{timeLeft}</Typography>
             <LinearProgress variant="determinate" value={barProgress} />
         </Card>
     );
+
+
+    
+}
+
+
+// return string of how many days, hours, min, sec, left.
+function getTimeLeftStr(endDateObj) {
+    const timeLeft = endDateObj - Date.now();
+
+    const msInDay = 1000 * 60 * 60 * 24;
+    const msInHour = 1000*60*60;
+    const msInMin = 1000*60;
+    const msInSecond = 1000;
+
+    const daysLeft = Math.floor(timeLeft / msInDay); // 1000ms -> 60 sec -> 60 min -> 24 hrs in a day
+    const daysLeftRemainder = timeLeft % msInDay;
+
+    const hoursLeft = Math.floor(daysLeftRemainder / msInHour);
+    const hoursLeftRemainder = daysLeftRemainder % msInHour;
+
+    const minsLeft = Math.floor(hoursLeftRemainder / msInMin);
+    const minsLeftRemainder = hoursLeftRemainder % msInMin;
+
+    const secondsLeft = Math.floor(minsLeftRemainder / msInSecond);
+
+    
+
+    return (`${daysLeft} days, ${hoursLeft} hours, ${minsLeft} mins, ${secondsLeft} seconds left`);
 
 
     
