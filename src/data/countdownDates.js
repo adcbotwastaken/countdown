3d75960f3ec80b4bf0db2ebeb461660db3f2777c
Timothy <lai.timothy77@gmail.com>
2021-10-03 21:08:33 -0400

    commented out events that already passed

--- src/data/countdownDates.js
+++ src/data/countdownDates.js
@@ -1,27 +1,27 @@
 const countdownDates = [
-    {
-        "name": "LOONA: Not Friends",
-        "startDate": '01 Sep 2021 00:00:00 EST',
-        "endDate": '2021-09-03T18:00:00.000+09:00'
-    },
+    // {
+    //     "name": "LOONA: Not Friends",
+    //     "startDate": '01 Sep 2021 00:00:00 EST',
+    //     "endDate": '2021-09-03T18:00:00.000+09:00'
+    // },
 
-    {
-        "name": "LOONA: Hula Hoop",
-        "startDate": '01 Sep 2021 00:00:00 EST',
-        "endDate": '2021-09-15T00:00:00.000+09:00'
-    },
+    // {
+    //     "name": "LOONA: Hula Hoop",
+    //     "startDate": '01 Sep 2021 00:00:00 EST',
+    //     "endDate": '2021-09-15T00:00:00.000+09:00'
+    // },
 
-    {
-        "name": "TWICE: The Feels",
-        "startDate": '23 Aug 2021 00:00:00 EST',
-        "endDate": '2021-10-01T13:00:00.000+09:00'
-    },
+    // {
+    //     "name": "TWICE: The Feels",
+    //     "startDate": '23 Aug 2021 00:00:00 EST',
+    //     "endDate": '2021-10-01T13:00:00.000+09:00'
+    // },
 
-    {
-        "name": "ITZY: CRAZY IN LOVE",
-        "startDate": '12 Aug 2021 00:00:00 EST',
-        "endDate": '2021-09-24T13:00:00.000+09:00'
-    },
+    // {
+    //     "name": "ITZY: CRAZY IN LOVE",
+    //     "startDate": '12 Aug 2021 00:00:00 EST',
+    //     "endDate": '2021-09-24T13:00:00.000+09:00'
+    // },
 
     {
         "name": "End of Year",
